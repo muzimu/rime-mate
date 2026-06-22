ccd39e96b5e354a17728a606746c7c4338ebb269
muzimu <2404580981@qq.com>
2026-06-22 16:28:02 +0800

    chore: 更新模型CNB_URL

--- module/ohMyRime/resources.go
+++ module/ohMyRime/resources.go
@@ -3,5 +3,5 @@ package ohMyRime
 // urls for resources
 const (
 	LMDG_GITHUB_URL = "https://github.com/amzxyz/RIME-LMDG/releases/download/LTS/wanxiang-lts-zh-hans.gram"
-	LMDG_CNB_URL    = "https://cnb.cool/Mintimate/rime/oh-my-rime/-/releases/download/latest/wanxiang-lts-zh-hans.gram"
+	LMDG_CNB_URL    = "https://cnb.cool/amzxyz/rime-wanxiang/-/releases/download/model/wanxiang-lts-zh-hans.gram"
 )
